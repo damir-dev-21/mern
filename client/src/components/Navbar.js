f12b03104c54a84fa543cd6f7d4c9a0fef860b77
damir-dev-21 <devdamiricher@gmail.com>
2021-01-27 12:45:39 +0500

    init

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import {NavLink,useHistory} from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+
+export const Navbar = () =>{
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+
+    const logoutHandler = (event) =>{
+        event.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+
+    return(
+        <nav>
+    <div class="nav-wrapper">
+      <a href="/" class="brand-logo">Сокращение</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><NavLink to="/create">Создать</NavLink></li>
+        <li><NavLink to="/links">Ссылки</NavLink></li>
+        <li><a href="/" onClick={logoutHandler}>Выйти</a></li>
+
+      </ul>
+    </div>
+  </nav>
+    )
+}
---
+++ client/src/pages/CreatePage.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook'
+import {useHistory} from 'react-router-dom'
+
+export const CreatePage = () =>{
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+    const {request} = useHttp()
+    const [link,setLink] = useState('')
+
+    const pressHandler = async event => {
+        if(event.key === 'Enter'){
+            try{
+                const data = await request('/api/link/generate','POST',{from:link}, {
+                    Authorization:`Bearer ${auth.token}`
+                })
+                history.push(`/detail/${data.link._id}`)
+            }catch(e){}
+        }
+    }
+
+    return(
+        <div className="row">
+            <div className="col s8 offset-s2" style={{paddingTop:'2rem'}}>
+            <div className="input-field">
+                <input placeholder="Вставьте ссылку" value={link} id="link" type="text" onChange={e=>setLink(e.target.value)} onKeyPress={pressHandler}/>
+                <label htmlFor="link">Пароль</label>
+            </div>
+            </div>
+        </div>
+    )
+}
